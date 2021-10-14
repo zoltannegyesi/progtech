fb04a974c405f54a72b2a7f196fcb68ae732add7
Négyesi Zoltán <lblrazor@gmail.com>
2021-10-14 23:12:55 +0200

    created VO classes, basic UserInput and drawTable

--- torpedo/src/main/java/hu/nye/progtech/torpedo/Main.java
+++ torpedo/src/main/java/hu/nye/progtech/torpedo/Main.java
@@ -1,2 +1,17 @@
-package hu.nye.progtech.torpedo;public class Main {
+package hu.nye.progtech.torpedo;
+
+import hu.nye.progtech.torpedo.model.PlayerVO;
+import hu.nye.progtech.torpedo.model.TableVO;
+import hu.nye.progtech.torpedo.ui.UserInput;
+
+public class Main {
+    public static void main(String[] args) {
+        //TODO: jacocot visszakapcsolni/kikommentezni
+
+        UserInput userInput = new UserInput();
+        PlayerVO player = new PlayerVO(userInput.nameScanning());
+        System.out.println(player.getName());
+        TableVO table = new TableVO();
+        table.drawTable();
+    }
 }
