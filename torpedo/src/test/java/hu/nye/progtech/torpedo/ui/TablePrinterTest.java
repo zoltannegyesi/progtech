41aea35795727bfa15edd982003dba602d903f31
Négyesi Zoltán <lblrazor@gmail.com>
2021-10-17 23:54:11 +0200

    created test for tablePrinter class

--- torpedo/src/test/java/hu/nye/progtech/torpedo/ui/TablePrinterTest.java
+++ torpedo/src/test/java/hu/nye/progtech/torpedo/ui/TablePrinterTest.java
@@ -1,2 +1,57 @@
-package hu.nye.progtech.torpedo.ui;public class TablePrinterTest {
+package hu.nye.progtech.torpedo.ui;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+public class TablePrinterTest {
+
+
+    List<List<Character>> listSample = new ArrayList<>();
+
+    private final String tableSample = "   A B C D E F G H I J\n" +
+            "1  X X X X X X X X X X \n" +
+            "2  X X X X X X X X X X \n" +
+            "3  X X X X X X X X X X \n" +
+            "4  X X X X X X X X X X \n" +
+            "5  X X X X X X X X X X \n" +
+            "6  X X X X X X X X X X \n" +
+            "7  X X X X X X X X X X \n" +
+            "8  X X X X X X X X X X \n" +
+            "9  X X X X X X X X X X \n" +
+            "10 X X X X X X X X X X \n";
+
+    TablePrinter underTest;
+
+    @BeforeEach
+    public void setUp() {
+        ArrayList<Character> temp = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            temp.add('X');
+        }
+        for (int i = 0; i < 10; i++) {
+            listSample.add(temp);
+        }
+        underTest = new TablePrinter(10, listSample);
+    }
+
+    @Test
+    public void testDrawTableShouldPrintTheTableToTheScreen() {
+        // given
+
+        // when
+        String result = underTest.drawTable();
+
+        // then
+        assertEquals(tableSample, result);
+    }
+
 }
