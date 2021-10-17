28ad6aa6f432a2dfd4e3145429d5fd40e7da6d9a
Négyesi Zoltán <lblrazor@gmail.com>
2021-10-17 23:53:46 +0200

    modified ui classes and main class for stylecheck

--- torpedo/src/main/java/hu/nye/progtech/torpedo/Main.java
+++ torpedo/src/main/java/hu/nye/progtech/torpedo/Main.java
@@ -1,17 +1,48 @@
 package hu.nye.progtech.torpedo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import hu.nye.progtech.torpedo.model.PlayerVO;
 import hu.nye.progtech.torpedo.model.TableVO;
 import hu.nye.progtech.torpedo.ui.UserInput;
 
+/**
+ * Main class, to run the program.
+ */
+
 public class Main {
+
+    /**
+     * Run the program.
+     */
+
     public static void main(String[] args) {
-        //TODO: jacocot visszakapcsolni/kikommentezni
+        /*TODO:
+           - jacocot visszakapcsolni/kikommentezni
+           - amit lehet, classot és fieldet finallá tenni
+           - kiíratást nem a tablevo-ban megcsinálni
+           - a kiíratásnál nem x-eket kiírni, hanem úgy tileokat létrehozni
+           és azok value-ját kiíratni
+           - esetleg keretes táblázatot csinálni a kiíratásnál
+
+         */
 
         UserInput userInput = new UserInput();
         PlayerVO player = new PlayerVO(userInput.nameScanning());
-        System.out.println(player.getName());
         TableVO table = new TableVO();
-        table.drawTable();
+        // TablePrinter printer = new TablePrinter(table);
+        // System.out.println(printer.drawTable());
+
+        List<List<Character>> asd = new ArrayList<>();
+        ArrayList<Character> temp = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            temp.add('X');
+        }
+        for (int i = 0; i < 10; i++) {
+            asd.add(temp);
+        }
+        System.out.println(asd.toString());
+
     }
 }
--- torpedo/src/main/java/hu/nye/progtech/torpedo/ui/UserInput.java
+++ torpedo/src/main/java/hu/nye/progtech/torpedo/ui/UserInput.java
@@ -1,10 +1,20 @@
 package hu.nye.progtech.torpedo.ui;
 
+import java.util.Objects;
 import java.util.Scanner;
+
+/**
+ * UI class to read from the standard input.
+ */
 
 public class UserInput {
 
+
     Scanner scanner = new Scanner(System.in);
+
+    /**
+     * Scans the standard input for names.
+     */
 
     public String nameScanning() {
         System.out.print("Adja meg a jatekos nevet: ");
@@ -14,4 +24,28 @@ public class UserInput {
     /*public <T> T scanning () {
 
     }*/
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UserInput userInput = (UserInput) o;
+        return Objects.equals(scanner, userInput.scanner);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(scanner);
+    }
+
+    @Override
+    public String toString() {
+        return "UserInput{" +
+                "scanner=" + scanner +
+                '}';
+    }
 }
